04657ae18131853ea80c1755356f9e3f135f7782
Martin M <martin7mondaca@gmail.com>
2021-04-14 20:13:15 -0700

    added function to query to news api

---
+++ client/src/utils/newsAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BASEURL = "https://newsapi.org/v2/everything?q="
+const searchParam = "Apple"
+const dateRange = "&from=2021-04-15&sortBy=popularity&apiKey="
+const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+
+
+export default {
+    search: function (query) {
+        return axios.get(
+            BASEURL + query + dateRange + API_KEY
+        );
+    }
+};
