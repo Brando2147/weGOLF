866e7196ec5517170d8812f44a1871db7d9d1281
Joshua Arroyo <Jalexarroyo@gmail.com>
2021-04-14 21:58:12 -0700

    fixed package.json files and am connected to the front end

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,16 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+var express = require('express');
+var router = express.Router();
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
+
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// module.exports = router;
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
@@ -10,7 +18,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
   });
 
   app.get("/login", (req, res) => {
