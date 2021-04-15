fc2b92ffd672cf6e4cb16ed9db8d54a31122d09d
Martin M <martin7mondaca@gmail.com>
2021-04-14 21:18:03 -0700

    updated route for landing page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,13 +4,13 @@ const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
   });
 
   app.get("/login", (req, res) => {
@@ -18,12 +18,12 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
   });
 };
