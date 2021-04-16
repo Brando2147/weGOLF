af3ace02fafb11ac3fa7ded738ed717fb260aca3
Martin M <martin7mondaca@gmail.com>
2021-04-15 20:19:58 -0700

    added router to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login.js"
+import Signup from "./pages/Signup.js"
 
 function App() {
   return (
     <>
-      <Login />
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+      </Router>
     </>
   );
 }
