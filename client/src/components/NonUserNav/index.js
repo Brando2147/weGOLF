b4cf18a733d32d13951cb1304b1d44fce695c4a6
Martin M <martin7mondaca@gmail.com>
2021-04-16 01:02:26 -0700

    updated styling and added bottom margin

--- client/src/components/NonUserNav/index.js
+++ client/src/components/NonUserNav/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style.css"
 
 
 function NonUserNav() {
     return (
-        <nav>
-            <Link to="/">
-                weGOLF
-            </Link>
+        <nav className="navbar is-success" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <Link className="navbar-item" to="/">
+                    weGOLF
+                </Link>
+            </div>
+
         </nav >
     )
 }
