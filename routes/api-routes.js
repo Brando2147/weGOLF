65db51c82b065506f461995b62bb8b47de0213d5
Brando D <brando2147@gmail.com>
2021-04-15 22:02:46 -0700

    Added API routes to get course data/signup for account

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,69 @@ const db = require("../models");
 const passport = require("../config/passport");
 
 module.exports = function(app) {
-  // Using the passport.authenticate middleware with our local strategy.
+  
+
+  //****************** GET ROUTES ****************** /
+
+  // Route for logging user out
+  app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", (req, res) => {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
+  });
+};
+
+
+// GET route to retrieve data for specific state AND city sorted by ascending state
+app.get("/api/posts/locationAscending/:city/:state", function (req, res) {
+  db.Courses.findAll({
+    where: {
+      courseCity: req.params.city,
+      courseState: req.params.state,
+    },
+    order: [["courseState", "ASC"]],
+  }).then(function (dbPost) {
+    res.json(dbPost);
+  });
+});
+
+
+// GET route to find all cities that have been added to the database
+app.get("/api/city", function (req, res) {
+  db.Courses.findAll({
+    attributes: ["courseCity"],
+  }).then(function (dbPost) {
+    res.json(dbPost);
+  });
+});
+
+// GET route to find all states that have been added to the database
+app.get("/api/state", function (req, res) {
+  db.Courses.findAll({
+    attributes: ["courseState"],
+  }).then(function (dbPost) {
+    res.json(dbPost);
+  });
+});
+
+
+  //****************** POST ROUTES ****************** /
+
+// Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
@@ -31,24 +93,23 @@ module.exports = function(app) {
       });
   });
 
-  // Route for logging user out
-  app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
+// Route to sign up for an account 
+  app.post("/api/signup", (req, res) => {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      city: req.body.city,
+      state: req.body.state,
+      zip: req.body.zip,
+    })
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
+      .catch((err) => {
+        res.status(401).json(err.message);
+      });
   });
 
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", (req, res) => {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
-};
+
+    //****************** PUT ROUTES ****************** /
