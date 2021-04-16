3039dc16d9afed965e75b946341188b58c2bf365
Martin M <martin7mondaca@gmail.com>
2021-04-16 01:01:44 -0700

    updated styling of footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,20 +1,17 @@
 import React from "react";
+import "./style.css"
 
 function Footer() {
     return (
-        <footer>
+        <footer className="footer">
             <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h4>Collaborators</h4>
-                        <p>
-                            <a href="https://github.com/abgabbard" target="_blank" rel="noreferrer">Andrew Gabbard</a> |
-                            <a href="https://github.com/Brando2147" target="_blank" rel="noreferrer">Brandon Dana</a> |
+                <h4>Collaborators</h4>
+                <p>
+                    <a href="https://github.com/abgabbard" target="_blank" rel="noreferrer">Andrew Gabbard</a>
+                    | <a href="https://github.com/Brando2147" target="_blank" rel="noreferrer">Brandon Dana</a> |
                             <a href="https://github.com/JAA459" target="_blank" rel="noreferrer">Joshua Arroyo</a> |
                             <a href="https://github.com/martinmondaca" target="_blank" rel="noreferrer">Martin Mondaca</a>
-                        </p>
-                    </div>
-                </div>
+                </p>
             </div>
         </footer>
     )
