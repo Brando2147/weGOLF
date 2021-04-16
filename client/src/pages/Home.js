378b16b56dd1ff638d94c421c5385139534efe23
Martin M <martin7mondaca@gmail.com>
2021-04-15 20:19:39 -0700

    added Home page with nav and footer compoentns, and news feed

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import UserFooter from "../components/UserFooter/index.js";
+import UserNav from "../components/UserNav/index.js"
+import NewsFeed from "../components/NewsFeed/index.js"
+
+function Home() {
+    return (
+        <>
+            <UserNav />
+            <NewsFeed />
+
+            <UserFooter />
+        </>
+    )
+}
+
+export default Home;
