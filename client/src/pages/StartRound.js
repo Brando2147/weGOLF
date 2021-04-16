4413952e0b7cd3824c0df231e4a908793e721038
Martin M <martin7mondaca@gmail.com>
2021-04-15 20:17:29 -0700

    started page for start round component, will add areas for users to input city, state, and players

---
+++ client/src/pages/StartRound.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import UserNav from "../components/UserNav/index.js"
+
+class StartRound extends Component {
+
+    render() {
+        return (
+            <>
+                <UserNav />
+                <form>
+
+                </form>
+            </>
+        )
+    }
+}
+
+export default StartRound;
