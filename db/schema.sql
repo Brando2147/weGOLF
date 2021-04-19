6908aa6d6d32a9efd66c72237a3c693e634a9ddf
Joshua Arroyo <Jalexarroyo@gmail.com>
2021-04-18 22:18:12 -0700

    changed models to include only 3

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,15 @@ insert into scores (holeOne, holeTwo, holeThree)
 VALUES (4,2,3);
 
 insert into courses (courseId, courseName, courseCity, courseState)
-VALUES (3, "LoneTree", "Gilbert", "Arizona")
+VALUES (3, "LoneTree", "Gilbert", "Arizona");
 
 insert into report (UserId, CourseId, ScoreId)
-VALUES (1,1,1)
+VALUES (1,1,1);
+
+insert into player (playerName)
+VALUES ("Joshua");
+insert into player (playerName)
+VALUES ("Andrew")
 
 SELECT firstName, lastName, holeOne, holeTwo, holeThree, courseName, courseCity, courseState
 From user
