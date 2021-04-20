749554d97d26993a22bb0a19496158abb81a3af2
Glimpz <abgabbard@gmail.com>
2021-04-19 20:25:31 -0700

    added help image walkthrough

--- client/src/pages/Help.js
+++ client/src/pages/Help.js
@@ -4,13 +4,14 @@ import UserFooter from "../components/UserFooter/index.js";
 import UserNav from "../components/UserNav/index.js"
 import NewsFeed from "../components/NewsFeed/index.js"
 import Home from "../pages/Home.js";
+import HelpIMG from "../utils/images/weGOLFHelp.png"
 
 
 function Help() {
     return (
         <>
             <UserNav />
-
+                <img src={HelpIMG} alt="Help image"/>
             <UserFooter />
         </>
     )
