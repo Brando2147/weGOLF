8003fa4e9f72c43aed420a30b477c829163e70f9
Brando D <brando2147@gmail.com>
2021-04-19 20:27:32 -0700

    Added put route to update scorecards, added ScoreCard/PlayerScoreCard components

--- client/src/components/Scorecard/index.js
+++ client/src/components/Scorecard/index.js
@@ -1,57 +1,69 @@
 import React from "react";
+import React, { useState } from "react";
+
+// const ScoreCard = (props) => {
+//   const { roundId, players = [], holes = [] } = props
+//   return (
+//     players.map(player => <PlayerScoreCard playerId={player} roundId={roundid} holes={player.holes} />)
+//   )
+// }
 
 function Scorecard(props) {
+  console.log(props);
 
-    console.log(props)
+  let numOfHoles = parseInt(props.details.numOfHoles);
+  let numOfHolesArr = [...Array(numOfHoles)].map((_, i) => i + 1);
 
-    let numOfHoles = parseInt(props.details.numOfHoles)
-    let numOfHolesArr = [...Array(numOfHoles)].map((_, i) => i + 1);
+  // let numOfPlayers = parseInt(props.details.numOfPlayers)
+  // let numOfPlayersArr = [...Array(numOfPlayers)].map((_, i) => i);
 
-    // let numOfPlayers = parseInt(props.details.numOfPlayers)
-    // let numOfPlayersArr = [...Array(numOfPlayers)].map((_, i) => i);
+  // console.log(props.details.playerNameArr)
 
-    // console.log(props.details.playerNameArr)
-
-
-    return (
-        <>
-            <p>Course Name: {props.details.roundCourseName} | Location: {props.details.roundCity}, {props.details.roundState}</p>
-            <div className="table-container container column is-10">
-
-                <table className="table is-bordered is-hoverable is-fullwidth">
-                    <thead>
-                        <tr>
-                            <th>Player/Hole</th>
-                            {numOfHolesArr.map((each, index) => (
-
-                                <td id={"hole" + (each)} className="">{each}</td>
-                            )
-                            )}
-                            <td>Total</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.details.playerNameArr.map((playerName, i) => (
-
-                            <tr id={playerName} player={"player" + (i + 1)} key={playerName}>
-                                <th id={playerName} player={"player" + (i + 1)}>{playerName}</th>
-                                {numOfHolesArr.map((each, index) => (
-
-                                    <td id={playerName} hole={"hole" + each} player={"player" + (i + 1)} key={each} contentEditable="true"></td>
-                                )
-                                )}
-                                <td id={playerName + "Total"}></td>
-                            </tr>
-                        ))}
-
-                    </tbody>
-                </table>
-                <button>Suspend Round</button>
-                <button>End Round</button>
-            </div>
-        </>
-    )
-
+  return (
+    <>
+      <p>
+        Course Name: {props.details.roundCourseName} | Location:{" "}
+        {props.details.roundCity}, {props.details.roundState}{" "}
+        {props.details.RoundId}
+      </p>
+      <div className="table-container container column is-10">
+        <table className="table is-bordered is-hoverable is-fullwidth">
+          <thead>
+            <tr>
+              <th>Player/Hole</th>
+              {numOfHolesArr.map((each, index) => (
+                <td id={"hole" + each} className="">
+                  {each}
+                </td>
+              ))}
+              <td>Total</td>
+            </tr>
+          </thead>
+          <tbody>
+            {props.details.playerNameArr.map((playerName, i) => (
+              <tr id={playerName} player={"player" + (i + 1)} key={playerName}>
+                <th id={playerName} player={"player" + (i + 1)}>
+                  {playerName}
+                </th>
+                {numOfHolesArr.map((each, index) => (
+                  <td
+                    id={playerName}
+                    hole={"hole" + each}
+                    player={"player" + (i + 1)}
+                    key={each}
+                    contentEditable="true"
+                  ></td>
+                ))}
+                <td id={playerName + "Total"}></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button>Suspend Round</button>
+        <button>End Round</button>
+      </div>
+    </>
+  );
 }
 
-export default Scorecard
+export default Scorecard;
