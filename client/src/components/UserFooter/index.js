4e2c4a870b99b247664ce90c2d4c33c3ce81ebeb
Martin M <martin7mondaca@gmail.com>
2021-04-21 15:17:28 -0700

    combined both footer into a single footer component

--- client/src/components/UserFooter/index.js
+++ client/src/components/UserFooter/index.js
@@ -4,7 +4,15 @@ import "./style.css"
 function UserFooter() {
     return (
         <footer className="footer">
-            <div className="content has-text-centered">
+            <div className="container">
+                <h4>Collaborators</h4>
+                <p>
+                    <a href="https://github.com/abgabbard" target="_blank" rel="noreferrer">Andrew Gabbard</a>
+                    | <a href="https://github.com/Brando2147" target="_blank" rel="noreferrer">Brandon Dana</a> |
+                            <a href="https://github.com/JAA459" target="_blank" rel="noreferrer">Joshua Arroyo</a> |
+                            <a href="https://github.com/martinmondaca" target="_blank" rel="noreferrer">Martin Mondaca</a>
+                </p>
+                <hr />
                 <a href="https://github.com/Brando2147/weGOLF" target="_blank" className="fab fa-github fa-3x"></a>
             </div>
         </footer>
