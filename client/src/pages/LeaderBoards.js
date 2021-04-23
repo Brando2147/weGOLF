5767989e157190f03f923412163c59d4d1e27105
Glimpz <abgabbard@gmail.com>
2021-04-23 15:03:21 -0700

    able to append player names to page. Need to fix the rest of the page and table along with navbar

--- client/src/pages/LeaderBoards.js
+++ client/src/pages/LeaderBoards.js
@@ -4,73 +4,41 @@ import { Link } from "react-router-dom";
 import UserNav from "../components/UserNav/index.js";
 import NewsFeed from "../components/NewsFeed/index.js";
 import Home from "../pages/Home.js";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// <tr class="is-selected">
-// <th>1</th>
-// <td class="Top1"> Martin M <strong>(W)</strong></td>
-// <td class="Low">-6</td>
-// <td class="CourseName">The Links</td>
-// <td class="City">San Diego</td>
-// <td class="State">CA</td>
-// </tr>
+let emptyArray = [];
 
 function LeaderBoards() {
-  // const [state, setState] = useState({
-  //   playerName: "",
-  // });
-
-var leaders = () => {
-return axios({
-      method: "GET",
-      url: `/api/leaderboards`,
-    }).then((data) => {
-      const leaderData = data.data;
-      leaderData.map((ld) => {
-        return (
-          <tr>{ld.playerName}</tr>
-        )
+  const [players, setPlayers] = useState([
+    {
+      playerName: '',
+    }
+  ]);
+  useEffect(() => {
+    var leaders = () => {
+      axios({
+        method: "GET",
+        url: `/api/leaderboards`,
+      }).then((data) => {
+        const leaderData = data.data;
+       setPlayers(leaderData.map(leadername =>
+        {
+return {
+  playerName: leadername.playerName
+}
+        }));
       });
-    });
-  
-  };
-  
+    };
+    leaders();
+  }, []);
+  console.log("players:", players)
   return (
-    <>
-      <UserNav />
-      <div class="columns is-centered is-mobile">
-        <div class="row">
-          <div class="column">
-            <table class="table is-striped is-bordered">
-              <thead>
-                <tr>
-                  <th>
-                    <abbr title="Rank">Rank</abbr>
-                  </th>
-                  <th>User (Player) </th>
-                  <th>
-                    <abbr title="Score">Best Round</abbr>
-                  </th>
-                  <th>
-                    <abbr title="CourseName">Course</abbr>
-                  </th>
-                  <th>
-                    <abbr title="City">City</abbr>
-                  </th>
-                  <th>
-                    <abbr title="State">State</abbr>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>{leaders}</tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      {/* <UserFooter /> */}
-    </>
+    <div>{players.map(p =><div>{p.playerName}</div>)}</div>
+    // <>
+    //   <UserNav />
+    //   {/* <UserFooter /> */}
+    // </>
   );
 }
-
 export default LeaderBoards;
