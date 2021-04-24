9f6110729b1cd25f5e39c5a7038fa836925d1dcf
Glimpz <abgabbard@gmail.com>
2021-04-24 12:54:35 -0700

    getting user ID and recent match data sent correctly on recent matches page

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Login from "./pages/Login.js"
 import Signup from "./pages/Signup.js"
 import StartRound from "./pages/StartRound.js"
 import Home from "./pages/Home.js"
-import MatchHistory from "./pages/MatchHistory.js"
+import RecentMatches from "../src/components/RecentMatches/index.js"
 import Courses from "./pages/Courses"
 import LeaderBoards from './pages/LeaderBoards';
 import MyAccount from './pages/MyAccount';
@@ -49,7 +49,7 @@ function App() {
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/home" component={Home} />
             <Route exact path="/newmatch" component={StartRound} />
-            <Route exact path="/matchhistory" component={MatchHistory} />
+            <Route exact path="/RecentMatches"  > <RecentMatches user={user}/> </Route>
             <Route exact path="/courses" component={Courses} />
             <Route exact path="/leaderboards" component={LeaderBoards} />
             <Route exact path="/myaccount" component={MyAccount} />
--- client/src/components/RecentMatches/index.js
+++ client/src/components/RecentMatches/index.js
@@ -1,81 +1,98 @@
-import React, { useState } from "react";
-import PlayerMatchHistory from "../PlayerMatchHistory/index.js";
+// import React, { useState } from "react";
+import React, { useEffect, useState } from "react"
+// import PlayerMatchHistory from "../PlayerMatchHistory/index.js";
 import axios from "axios";
 import "./style.css";
 
 //State data to retrieve from /api/recent/
-const [state, setState] = useState({
-  playerName: "",
-  createdAt: "",
-  courseName: "",
-  courseCity: "",
-  courseState: "",
-  hole1: 0,
-  hole2: 0,
-  hole3: 0,
-  hole4: 0,
-  hole5: 0,
-  hole6: 0,
-  hole7: 0,
-  hole8: 0,
-  hole9: 0,
-  hole10: 0,
-  hole11: 0,
-  hole12: 0,
-  hole13: 0,
-  hole14: 0,
-  hole15: 0,
-  hole16: 0,
-  hole17: 0,
-  hole18: 0,
-});
-
-
-// GET call to retrieve recent matches from database
-axios({
-  method: "GET",
-  data: state,
-  url: `/api/recent/${props.user.uid}/`,
-});
+// const [state, setState] = useState({
+//   playerName: "",
+//   createdAt: "",
+//   courseName: "",
+//   courseCity: "",
+//   courseState: "",
+//   hole1: 0,
+//   hole2: 0,
+//   hole3: 0,
+//   hole4: 0,
+//   hole5: 0,
+//   hole6: 0,
+//   hole7: 0,
+//   hole8: 0,
+//   hole9: 0,
+//   hole10: 0,
+//   hole11: 0,
+//   hole12: 0,
+//   hole13: 0,
+//   hole14: 0,
+//   hole15: 0,
+//   hole16: 0,
+//   hole17: 0,
+//   hole18: 0,
+// });
 
 // Recent matches component copied from PlayerScoreCard component
-function RecentMatches() {
-  let numOfHoles = parseInt(props.details.numOfHoles);
-  let numOfHolesArr = [...Array(numOfHoles)].map((_, i) => i + 1);
-console.log(props)
-  return (
-    <>
-      <div className="table-container">
-        <table className="table is-bordered is-narrow is-hoverable">
-          <thead>
-            <tr>
-              <th>Player/Hole</th>
-              {numOfHolesArr.map((each, index) => (
-                <td hole={"hole" + each} key={"hole" + each} className="">
-                  {each}
-                </td>
-              ))}
-              <td>Total</td>
-            </tr>
-          </thead>
-          <tbody>
-            {props.details.playerNameArr.map((each, index) => {
-              return (
-                <PlayerScoreCard
-                  playerName={each}
-                  player={"player" + (index + 1)}
-                  playerID={props.details.playerIdArr[index]}
-                  roundId={props.details.roundId}
-                  holes={props.details.numOfHoles}
-                />
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+function RecentMatches({user}) {
+  useEffect(() =>{
+    console.log(user.uid, "Something")
+    axios({
+      method: "GET",
+      url: `/api/recent/${user.uid}/`,
+    }).then((result) => {
+      console.log(result)
+    })
+  }, [user])
+return (
+  <div>Some Text Here</div>
+)
+
 }
 
-
 export default RecentMatches;
+
+
+
+
+
+
+
+
+
+
+
+//Brandon's Original Code:
+//   let numOfHoles = parseInt(props.details.numOfHoles);
+//   let numOfHolesArr = [...Array(numOfHoles)].map((_, i) => i + 1);
+// console.log(props)
+//   return (
+//     <>
+//       <div className="table-container">
+//         <table className="table is-bordered is-narrow is-hoverable">
+//           <thead>
+//             <tr>
+//               <th>Player/Hole</th>
+//               {numOfHolesArr.map((each, index) => (
+//                 <td hole={"hole" + each} key={"hole" + each} className="">
+//                   {each}
+//                 </td>
+//               ))}
+//               <td>Total</td>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {props.details.playerNameArr.map((each, index) => {
+//               return (
+//                 <PlayerScoreCard
+//                   playerName={each}
+//                   player={"player" + (index + 1)}
+//                   playerID={props.details.playerIdArr[index]}
+//                   roundId={props.details.roundId}
+//                   holes={props.details.numOfHoles}
+//                 />
+//               );
+//             })}
+//           </tbody>
+//         </table>
+//       </div>
+//     </>
+//   );
