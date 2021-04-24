770c41f620831dbaf12453f120d829ab1c42b64f
Glimpz <abgabbard@gmail.com>
2021-04-24 13:37:07 -0700

    updating player names from recent matches to the page using a div

--- client/src/components/RecentMatches/index.js
+++ client/src/components/RecentMatches/index.js
@@ -4,8 +4,107 @@ import React, { useEffect, useState } from "react"
 import axios from "axios";
 import "./style.css";
 
+
+
+// Recent matches component copied from PlayerScoreCard component
+function RecentMatches({user}) {
+const [matchData, setMatchData] = useState([ {
+  playerName: "",
+  createdAt: "",
+  courseName: "",
+  courseCity: "",
+  courseState: "",
+  hole1: 0,
+  hole2: 0,
+  hole3: 0,
+  hole4: 0,
+  hole5: 0,
+  hole6: 0,
+  hole7: 0,
+  hole8: 0,
+  hole9: 0,
+  hole10: 0,
+  hole11: 0,
+  hole12: 0,
+  hole13: 0,
+  hole14: 0,
+  hole15: 0,
+  hole16: 0,
+  hole17: 0,
+  hole18: 0,
+}
+]);
+
+
+
+
+
+  useEffect(() =>{
+    console.log(user.uid, "Something")
+    axios({
+      method: "GET",
+      url: `/api/recent/${user.uid}/`,
+    }).then((result) => {
+      console.log(result)
+      const allMatches = result.data;
+      setMatchData(allMatches.map(matchArray => 
+        {
+          console.log(matchArray)
+        return {
+          playerName: matchArray.playerName,
+          createdAt: matchArray.createdAt,
+          courseName: matchArray.courseName,
+          courseCity: matchArray.courseCity,
+          courseState: matchArray.courseState,
+          hole1: matchArray.hole1,
+          hole2: matchArray.hole2,
+          hole3: matchArray.hole3,
+          hole4: matchArray.hole4,
+          hole5: matchArray.hole5,
+          hole6: matchArray.hole6,
+          hole7: matchArray.hole7,
+          hole8: matchArray.hole8,
+          hole9: matchArray.hole9,
+          hole1: matchArray.hole10,
+          hole11: matchArray.hole11,
+          hole12: matchArray.hole12,
+          hole13: matchArray.hole13,
+          hole14: matchArray.hole14,
+          hole15: matchArray.hole15,
+          hole16: matchArray.hole16,
+          hole17: matchArray.hole17,
+          hole18: matchArray.hole18,
+        }
+      }))
+    })
+  }, [user])
+
+
+
+
+
+
+  return (
+  <div>
+{matchData.map(md => <div>{md.playerName}</div>)}
+
+  </div>
+)
+
+}
+
+export default RecentMatches;
+
+
+
+
+
+
+
+
+
 //State data to retrieve from /api/recent/
-// const [state, setState] = useState({
+// const [matchData, setMatchData] = useState({
 //   playerName: "",
 //   createdAt: "",
 //   courseName: "",
@@ -30,35 +129,6 @@ import "./style.css";
 //   hole17: 0,
 //   hole18: 0,
 // });
-
-// Recent matches component copied from PlayerScoreCard component
-function RecentMatches({user}) {
-  useEffect(() =>{
-    console.log(user.uid, "Something")
-    axios({
-      method: "GET",
-      url: `/api/recent/${user.uid}/`,
-    }).then((result) => {
-      console.log(result)
-    })
-  }, [user])
-return (
-  <div>Some Text Here</div>
-)
-
-}
-
-export default RecentMatches;
-
-
-
-
-
-
-
-
-
-
 
 //Brandon's Original Code:
 //   let numOfHoles = parseInt(props.details.numOfHoles);
