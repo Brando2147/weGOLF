b1285e5604778beb06351269a88eab076b98b61e
Martin M <martin7mondaca@gmail.com>
2021-05-02 04:29:16 -0700

    resized help image and centered on page, also removed imports that were not being used

--- client/src/pages/Help.js
+++ client/src/pages/Help.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import UserFooter from "../components/UserFooter/index.js";
 import UserNav from "../components/UserNav/index.js"
-import NewsFeed from "../components/NewsFeed/index.js"
-import Home from "../pages/Home.js";
 import HelpIMG from "../utils/images/weGOLFHelp.png"
-
 
 function Help() {
     return (
         <>
             <UserNav />
-            <img src={HelpIMG} alt="Help image" />
-            {/* <UserFooter /> */}
+            <div className="column has-text-centered">
+                <h1 className="title is-1">Help</h1>
+            </div>
+            <div className="column is-centered has-text-centered">
+                <img src={HelpIMG} alt="Help image" width="750px" />
+            </div>
         </>
     )
 }
