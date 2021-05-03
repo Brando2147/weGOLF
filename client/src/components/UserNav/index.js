261d377dbc79a0e840bef107c38f99b2d6708b80
Brando D <brando2147@gmail.com>
2021-05-02 21:05:14 -0700

    Deploying to heroku, fixing syntax errors

--- client/src/components/UserNav/index.js
+++ client/src/components/UserNav/index.js
@@ -2,48 +2,24 @@ import React, { useState, useEffect } from "react";
 import { Link, Redirect, useHistory } from "react-router-dom";
 import firebase from "../../firebase";
 import "./style.css";
-
 function UserNav() {
   let history = useHistory();
-
   const [isActive, setisActive] = React.useState(false);
-
   const [user, setUser] = useState(false);
-
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        console.log(user.uid);
         setUser(user);
       } else {
         console.log("user is not signed in");
-
         history.push("/");
       }
     });
   }, []);
-
   const handleSignOut = () => {
     firebase.auth().signOut();
     setUser(false);
   };
-
-
-  const currentRoundCheck = function () {
-    axios({
-      method: "GET",
-      url: `/api/complete/${user.uid}/`,
-    }).then((result) => {
-      console.log(result.data);
-      if (result.data.length === 0) {
-        setRoute("/newMatch");
-      } else {
-        setRoute("/CurrentRound");
-      }
-    });
-  };
-  currentRoundCheck();
-
   return (
     <>
       <nav
@@ -55,7 +31,6 @@ function UserNav() {
           <Link className="navbar-item" to="/home">
             <h3>weGolf</h3>
           </Link>
-
           <a
             onClick={() => {
               setisActive(!isActive);
@@ -71,38 +46,32 @@ function UserNav() {
             <span aria-hidden="true"></span>
           </a>
         </div>
-
         <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
           <div className="navbar-end">
             <Link className="navbar-item is-tab" to="/newmatch">
-
               <span className="icon is-small">
                 <i className="fas fa-golf-ball" aria-hidden="true"></i>
               </span>
               <span> New Match</span>
             </Link>
-
             <Link className="navbar-item is-tab" to="/recentMatches">
               <span className="icon is-small">
                 <i className="fas fa-table" aria-hidden="true"></i>
               </span>
               <span>Recent Matches</span>
             </Link>
-
             <Link className="navbar-item is-tab" to="/leaderboards">
               <span className="icon is-small">
                 <i className="fas fa-trophy" aria-hidden="true"></i>
               </span>
               <span>Leaderboards</span>
             </Link>
-
             <Link className="navbar-item is-tab" to="/myaccount">
               <span className="icon is-small">
                 <i className="fas fa-user" aria-hidden="true"></i>
               </span>
               <span>My Account</span>
             </Link>
-
             <Link className="navbar-item is-tab" to="/help">
               <span className="icon is-small">
                 <i className="fas fa-question" aria-hidden="true"></i>
@@ -120,7 +89,5 @@ function UserNav() {
       </nav>
     </>
   );
-
 }
-
 export default UserNav;
